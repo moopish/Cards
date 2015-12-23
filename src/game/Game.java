0eb9f955ab5cd09eff5d5ff63df861662ebc584b
Moop <msvandyk@hotmail.com>
2015-12-23 07:30:02 -0500

    Added Game class, all games will follow this format

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,9 +1,18 @@
 package game;
 
+import cards.Deck;
+
 /**
  * Created by Moop on 2015-12-23.
  *
  */
-public interface Game {
-    void run();
+public abstract class Game {
+
+    protected final Deck deck;
+
+    public Game(Deck deck) {
+        this.deck = deck;
+    }
+
+    public abstract void run();
 }
